843365d6677d69dfe2c0fce1b9d7d5ee44bd7720
Karina-Blur <k.blurtsyan@st.hanze.nl>
2023-05-13 15:35:28 +0200

    scene manager

---
+++ Assets/Scripts/sceneone.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneone : MonoBehaviour
+{
+    public string p2cutscenetest; // Name of the scene you want to switch to
+    public float delayTime = 900f; // Delay time in seconds
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= delayTime)
+        {
+            ChangeScene();
+            Debug.Log("yez");
+        }
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(p2cutscenetest);
+    }
+}
---
+++ Assets/Scripts/scenetwo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenetwo : MonoBehaviour
+{
+    public string mainlvl; // Name of the scene you want to switch to
+    public float delayTime = 350f; // Delay time in seconds
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= delayTime)
+        {
+            ChangeScene();
+        }
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(mainlvl);
+    }
+}
