732ac1ef8609ee664d375fca0fdd4051d5c55cf9
Hufflepack <106102230+Hufflepack@users.noreply.github.com>
2023-05-23 19:16:09 +0200

    Stuff

---
+++ Assets/NPC.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+using TMPro;
+
+using UnityEngine.UI;
+
+using System.Collections;
+
+using System.Collections.Generic;
+
+using System;
+
+
+
+[CreateAssetMenu(fileName = "AI file", menuName = "AI Files Archive")]
+
+public class NPC : ScriptableObject
+
+{
+
+
+
+    public string name;
+
+    [TextArea(3, 15)]
+
+    public string[] dialogue;
+
+    [TextArea(3, 15)]
+
+    public string[] playerDialogue;
+
+
+
+}
+
---
+++ Assets/ObjectHighlight.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+public class ObjectHighlight : MonoBehaviour
+{
+
+    private float interactionRange = 5.0f;
+    private Camera playerCamera;
+    private Vector3 screenCenter;
+    private GameObject currentTarget;
+    private int defaultMask;
+    private int highlightMask;
+
+    public delegate void HoverNewObjectHandler(GameObject target);
+    public event HoverNewObjectHandler OnHoverNewObject;
+
+    public delegate void ExitHoverHandler(GameObject target);
+    public event ExitHoverHandler OnExitHover;
+
+    private void Awake()
+    {
+
+        screenCenter = new Vector3(Screen.width >> 1, Screen.height >> 1);
+        defaultMask = LayerMask.NameToLayer("Default");
+        highlightMask = LayerMask.NameToLayer("highlight");
+
+        
+    }
+
+    private void Update()
+    {
+
+        if (playerCamera == null)
+        {
+
+            playerCamera = FindCamera();
+
+        }
+
+        else if (currentlyPointing)
+        {
+
+            RaycastHit info;
+            if (Physics.Raycast(playerCamera.ScreenPointToRay(screenCenter), out info, interactionRange, LayerMask.GetMask("Default", "Highlight")))
+            {
+
+                GameObject target = info.collider.gameObject;
+
+                if (currentTarget != target)
+                {
+
+                    OnHoverNewObject?.Invoke(target);
+                    currentTarget = target;
+                    currentTarget.layer = highlightMask;
+
+                }
+
+            }
+            else if (currentTarget != null)
+            {
+
+                currentTarget.layer = defaultMask;
+                OnExitHover?.Invoke(currentTarget);
+                currentTarget = null;
+
+
+            }
+        }
+    }
+
+    private Camera FindCamera()
+    {
+        // Find and return the main camera in the scene
+        return Camera.main;
+    }
+
+    // Define the currentlyPointing variable
+    private bool currentlyPointing = true;
+
+}
