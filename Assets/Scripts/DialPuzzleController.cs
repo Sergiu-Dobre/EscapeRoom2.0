830ec384fd0e50cb29d65e97871e14ac8f5ba2af
Karina-Blur <k.blurtsyan@st.hanze.nl>
2023-05-31 02:46:31 +0200

    stuff

--- Assets/Scripts/DialPuzzleController.cs
+++ Assets/Scripts/DialPuzzleController.cs
@@ -7,17 +7,29 @@ public class DialPuzzleController : MonoBehaviour
     // Array of the four dials in the puzzle
     public DialController[] dials;
 
+    
+
     // Array of the correct order of the dials
     public int[] correctOrder;
 
     public GameObject GameObject;
 
-    //public GameObject lockcol;
+    //public GameObject otherGameObject;
+
+    public GameObject lockcol;
     public GameObject box;
+    public GameObject enablepick;
+    public GameObject lockobj;
+    //public GameObject camon;
+
+    public Camera alternativeCamera;
+    public Camera mainCamera;
 
     // Update is called once per frame
     void Update()
     {
+        
+
         // Check if the dials are in the correct order
         bool isCorrectOrder = true;
         for (int i = 0; i < dials.Length; i++)
@@ -37,8 +49,18 @@ public class DialPuzzleController : MonoBehaviour
             //lockobj.SetActive(false);
             
             box.SetActive(false);
+            enablepick.SetActive(true);
 
-            //lockcol.GetComponent<BoxCollider>().enabled = false;
+            //switch to main camera
+            //camon.SetActive(true);
+            lockobj.SetActive(false);
+            lockcol.SetActive(false);
+
+            alternativeCamera.enabled = false;
+            mainCamera.enabled = true;
+
+            //otherGameObject.GetComponent<BoxCollider>();
+            //Destroy(locktrigger.GetComponent<BoxCollider>());
         }
     }
 }
