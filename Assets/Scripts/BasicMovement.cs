aafae63284f187c2bd46e04aff08df2adf567c10
JackyGodLvL <serhio107@gmail.com>
2023-06-12 16:12:42 +0200

    clean code for gravity

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -5,26 +5,24 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public CharacterController controlls;
-    // callin character controller
-
+    //i am calling an already existing component"Character controller"
     public float speed = 12f;
-    //define speed variable
+    public float gravity = -9.81f;
     Vector3 velocity;
-    // degfine velocity vecor 3
 
     // Update is called once per frame
     void Update()
     {
-       
-        // x,z axes for movment speed
+        // we are gonna use the axis for movement obv x&z because we dont want to go up
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
-        //this way we move where our player "look"
-       
-
+        //this way the movement will be transformed depending where does our player "look"
+      
         controlls.Move(move * speed * Time.deltaTime);
-        // movement will not be affected by frames
+        //the move in the brackets is the Vector3 from above, the speed variable, and the time delta time so that the movement wont be affected by frames
+       
+        velocity.y += gravity * Time.deltaTime;
         controlls.Move(velocity * Time.deltaTime);
     }
 }
